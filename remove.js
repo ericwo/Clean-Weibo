7f422df7e6198a22a29f8d0472bcff5b7a127bbe
Eric Wu <eric@originatechina.com>
2012-12-22 16:37:34 +0800

    remove 8 divs in the weibo index page

--- remove.js
+++ remove.js
@@ -1,8 +1,25 @@
 (function () {
-	console.log("get remove.js");
 	var content_1 = document.getElementById("trustPagelet_recom_interestv5");
 	content_1.style.display = "none";
 
 	var content_2 = document.getElementById("pl_rightmod_ads35");
 	content_2.style.display = "none";
+
+	var content_3 = document.getElementById("trustPagelet_recom_allinonev5");
+	content_3.style.display = "none";
+
+	var content_4 = document.getElementById("trustPagelet_zt_hottopicv5");
+	content_4.style.display = "none";
+
+	var content_5 = document.getElementById("trustPagelet_recom_memberv5");
+	content_5.style.display = "none";
+
+	var content_6 = document.getElementById("pl_rightmod_noticeboard");
+	content_6.style.display = "none";
+
+	var content_7 = document.getElementById("pl_leftnav_app");
+	content_7.style.display = "none";
+
+	var content_8 = document.getElementById("trustPagelet_checkin_lotteryv5");
+	content_8.style.display = "none";
 })();
