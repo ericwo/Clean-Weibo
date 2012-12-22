ee32c0c6221005be5a1721600903fc8f0e8dab95
Eric Wu <eric@originatechina.com>
2012-12-22 16:17:22 +0800

    project demo

---
+++ remove.js
@@ -0,0 +1,8 @@
+(function () {
+	console.log("get remove.js");
+	var content_1 = document.getElementById("trustPagelet_recom_interestv5");
+	content_1.style.display = "none";
+
+	var content_2 = document.getElementById("pl_rightmod_ads35");
+	content_2.style.display = "none";
+})();
