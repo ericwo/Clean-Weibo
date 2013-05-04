446ed4f61827dc5af6f26db2a76198b0cf50cb06
Eric Wu <eric@originatechina.com>
2013-05-04 10:21:32 +0800

    remove footer_advbg

--- remove.js
+++ remove.js
@@ -1,7 +1,8 @@
 (function () {
 	var blocksId = ["trustPagelet_recom_interestv5", "pl_rightmod_ads35", "trustPagelet_recom_allinonev5",
 									"trustPagelet_zt_hottopicv5", "trustPagelet_recom_memberv5", "pl_rightmod_noticeboard",
-									"pl_leftnav_app", "trustPagelet_checkin_lotteryv5", "pl_rightmod_ads36", "ad_37700"];
+									"pl_leftnav_app", "trustPagelet_checkin_lotteryv5", "pl_rightmod_ads36", "ad_37700",
+                  "footer_advbg"];
 
 	for (var i = 0, length = blocksId.length; i < length; i++) {
 		document.getElementById(blocksId[i]).style.display = "none";
