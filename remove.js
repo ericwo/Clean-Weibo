fae7efae8219c92e4e5068a5839fccc10e8b287c
Eric Wu <ericwu1989@gmail.com>
2013-08-05 21:55:12 +0800

    clean function

--- remove.js
+++ remove.js
@@ -1,7 +1,12 @@
 (function () {
-	var blocksId = ["trustPagelet_indexright_recom", "trustPagelet_recom_memberv5", "pl_rightmod_ads36", "pl_rightmod_noticeboard", "level_3_Box W_func", "pg_content_biztips"];
 
-	for (var i = 0, length = blocksId.length; i < length; i++) {
-		document.getElementById(blocksId[i]).style.display = "none";
-	}
+  var blocksId = ["trustPagelet_indexright_recom", "trustPagelet_recom_memberv5", "pl_rightmod_ads36", "pl_rightmod_noticeboard", "level_3_Box W_func", "pg_content_biztips"];
+
+  function clean() {
+    for (var i = 0, length = blocksId.length; i < length; i++) {
+      document.getElementById(blocksId[i]).style.display = "none";
+    }
+  }
+
+  clean();
 })();
